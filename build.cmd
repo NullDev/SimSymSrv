8b75a9946b7df2351216f34567bffeb8e5d339af
NullDev (Shadow) <chris@nulldev.org>
2025-06-10 13:56:10 +0200

    made it pretty

--- build.cmd
+++ build.cmd
@@ -1,2 +1,4 @@
-@echo off
+@echo off & setlocal EnableDelayedExpansion
+echo [SimSymSrv] Building...
 zig build-exe src/main.zig -O ReleaseFast -target x86_64-windows -fstrip -femit-bin="SimSymSrv.exe"
+echo [SimSymSrv] Done.
